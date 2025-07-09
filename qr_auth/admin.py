4aa3d6cab30013d44e417565278c27675892056c
Umid <umidx27@gmail.com>
2025-07-09 14:31:01 +0500

    Room modeliga telegram_chat_id maydoni qo'shildi

--- qr_auth/admin.py
+++ qr_auth/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Room, User
 
-# Register your models here.
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'number',
+        'telegram_chat_id',
+        'is_active',
+        'check_in',
+        'check_out',
+    )
+    list_editable = ('telegram_chat_id', 'is_active')
+    search_fields = ('number',)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'telegram_id', 'phone')
